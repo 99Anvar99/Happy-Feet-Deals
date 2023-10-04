3dcfe5f36e19bbf849bafd074e220ef33399dcdb
Ayush <ayushmanisharma@gmail.com>
2023-10-04 19:40:51 -0400

    reinitiated apollo connection

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
 const path = require('path');
 const express = require('express');
-const { ApolloServer } = require('@apollo/server');
+const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
