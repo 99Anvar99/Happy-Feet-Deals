0740d26ce3fa7a20a18e6b3f9e09b3c8d11eafff
Anvar <anvar.ganiev99@gmail.com>
2023-10-04 21:11:26 -0400

    Added Home.js and css

---
+++ client/src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/home.css'
+
+function home() {
+  return (
+    <div className="home">
+      <h1 id="homeTitle">Home</h1>
+    </div>
+  )
+}
+
+export default home
