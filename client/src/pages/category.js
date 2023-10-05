afb8126d2e32b25366b1446c0ae40bc7a74e241e
Anvar <anvar.ganiev99@gmail.com>
2023-10-05 18:49:46 -0400

    added category page and css

---
+++ client/src/pages/category.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/category.css'
+
+function category() {
+  return (
+    <div className="category">
+      <h1 id="categoryTitle">Category</h1>
+    </div>
+  )
+}
+
+export default category
