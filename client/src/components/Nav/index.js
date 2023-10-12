29b65a66149be35f2a451ede2e03e052d7879a71
Anvar <anvar.ganiev99@gmail.com>
2023-10-12 19:17:01 -0400

    fixed label

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -43,9 +43,7 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-         
-          <span role="img" aria-label="shopping bag">   👀...👀...  </span>
-          Happy-Feet-Shop ...👀...👀
+          Happy-Feet-Deals
         </Link>
       </h1>
 
