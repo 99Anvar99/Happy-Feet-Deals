b50f61daa9e04d55129cc521881858669fa9d9ba
Hung Quoc Dang <hungquocdang>
2023-10-14 03:20:08 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/happyfeet-shopping');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/happyfeet-shoppingb');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/HappyFeetDeals');
 
 module.exports = mongoose.connection;
