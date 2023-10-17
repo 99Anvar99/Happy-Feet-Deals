24e92ae5354b28a4e001a86e72a9169380f62f11
HungQuocDang <hungquoc016@gmail.com>
2023-10-17 19:55:06 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,7 +6,11 @@ const mongoose = require('mongoose');
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://hungb:Test1234@cluster0.pv7ehpy.mongodb.net/');
 
 /////////this line works with heroku shielded-harbor-32787
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Hung1:Test1234@cluster0.xpplqfx.mongodb.net/happyfeet-shoppinga?retryWrites=true&w=majority');
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Hung1:Test1234@cluster0.xpplqfx.mongodb.net/happyfeet-shoppinga?retryWrites=true&w=majority');
 /////////
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://hungoct17:Test1234@cluster0.b00xfaa.mongodb.net/');
+
 
 module.exports = mongoose.connection;
