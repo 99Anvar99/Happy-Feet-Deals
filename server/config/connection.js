7401e80ace0ebd8d4cd7ab36dc68467e916052b1
HungQuocDang <hungquoc016@gmail.com>
2023-10-17 01:58:49 -0400

    updates

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/happyfeet-shoppingb');
+ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/happyfeet-shoppingb');
 
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/happyfeet-shoppinga');
 // seeded to MongoDB Atlas cloub
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Hung1:LKI1G4P6rZKZJZlz@cluster0.rqdogfy.mongodb.net/?retryWrites=true&w=majority');
 
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://hungb:Test1234@cluster0.pv7ehpy.mongodb.net/');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://HappyFeetDeals:Test123@cluster0.xpplqfx.mongodb.net/');
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://HappyFeetDeals:Test123@cluster0.xpplqfx.mongodb.net/');
 
 module.exports = mongoose.connection;
